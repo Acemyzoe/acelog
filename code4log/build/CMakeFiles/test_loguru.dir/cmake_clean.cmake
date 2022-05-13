893458edd5f1cef05d0433819b5dfcd3274d4da6
acemyzoe <acemyzoe@outlook.com>
2022-05-13 15:46:44 +0800

    loguru test

---
+++ code4log/build/CMakeFiles/test_loguru.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_loguru.dir/src/test_loguru.cpp.o"
+  "test_loguru"
+  "test_loguru.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_loguru.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- code4log/src/test_loguru.cpp
+++ code4log/src/test_loguru.cpp
@@ -1,28 +1,34 @@
 
 #include <iostream>
-#define LOGURU_WITH_STREAMS 1
-#include "loguru/loguru.hpp"
-int testloguru()
+#define LOGURU_WITH_STREAMS 1 //使用流进行日志记录
+#include "loguru/loguru.cpp"
+int testloguru(int argc, char **argv)
 {
+    loguru::init(argc, argv);
     char ch = 'a';
     char str[10] = "AC";
     float float_val = 10.10;
     int num = 88;
     double double_val = 10.123456;
-    char log_path[1024];
-    loguru::suggest_log_path("./logurufile/", log_path, sizeof(log_path));       //获取日志文件路径
-    loguru::add_file(log_path, loguru::FileMode::Append, loguru::Verbosity_MAX); //设置日志文件路径
 
-    LOG_F(INFO, "字符为 %c \n", ch);
-    LOG_F(WARNING, "字符串为 %s \n", str);
-    LOG_F(ERROR, "浮点数为 %f \n", float_val);
+    char log_path[1024];
+    loguru::suggest_log_path("./logurufile/", log_path, sizeof(log_path)); //获取日志文件夹路径
+    loguru::add_file(log_path, loguru::FileMode::Append, loguru::Verbosity_MAX);
+
+    loguru::add_file("latest_readable.log", loguru::Append, loguru::Verbosity_INFO);
+
+    LOG_SCOPE_FUNCTION(INFO); //缩进日志记录以使其更具可读性
+    LOG_F(INFO, "字符为 %c ", ch);
+    LOG_F(WARNING, "字符串为 %s ", str);
+    LOG_F(ERROR, "浮点数为 %f ", float_val);
+
     LOG_S(INFO) << "整数为" << num;
     LOG_S(ERROR) << "双精度值为" << double_val;
     return 0;
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    testloguru();
+    testloguru(argc, argv);
     return 0;
 }
